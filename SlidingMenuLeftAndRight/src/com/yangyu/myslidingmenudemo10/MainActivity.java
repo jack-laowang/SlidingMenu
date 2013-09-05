7e79d0e8f611e5c3fd2a716421ca0c5ce69f0d14
wangmq <1131212885@qq.com>
2013-09-05 17:17:14 +0800

    SlidingMenu
    SlidingMenu

---
+++ SlidingMenuLeftAndRight/src/com/yangyu/myslidingmenudemo10/MainActivity.java
@@ -0,0 +1,93 @@
+package com.yangyu.myslidingmenudemo10;
+
+import android.annotation.SuppressLint;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.animation.Interpolator;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu.CanvasTransformer;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+
+@SuppressLint("NewApi")
+public class MainActivity extends SlidingFragmentActivity {
+	private CanvasTransformer mTransformer;
+		
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setTitle("Slide Up");
+		
+		initAnimation();
+		
+		initSlidingMenu();
+		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	/**
+	 * 初始化滑动菜单
+	 */
+	private void initSlidingMenu(){
+		SlidingMenu sm = getSlidingMenu();	
+		sm.setMode(SlidingMenu.LEFT_RIGHT);
+		// 设置主界面视图
+		setContentView(R.layout.content_frame);
+		getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new SampleListFragment()).commit();
+				
+		// 设置滑动菜单视图
+		setBehindContentView(R.layout.menu_frame);
+		getSupportFragmentManager().beginTransaction().replace(R.id.menu_frame, new SampleListFragment()).commit();
+
+		sm.setSecondaryMenu(R.layout.menu_frame_two);
+		sm.setSecondaryShadowDrawable(R.drawable.shadowright);
+		getSupportFragmentManager().beginTransaction().replace(R.id.menu_frame_two, new SampleListFragment()).commit();
+		
+		
+		// 设置滑动菜单的属性值
+		sm.setShadowWidthRes(R.dimen.shadow_width);
+		sm.setShadowDrawable(R.drawable.shadow);
+		sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+		sm.setFadeDegree(0.35f);
+		sm.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+		sm.setBehindScrollScale(0.0f);
+		sm.setBehindCanvasTransformer(mTransformer);
+		
+		setSlidingActionBarEnabled(true);
+	}
+
+	private static Interpolator interp = new Interpolator() {
+		@Override
+		public float getInterpolation(float t) {
+			t -= 1.0f;
+			return t * t * t + 1.0f;
+		}		
+	};
+	
+	/**
+	 * 初始化动画效果
+	 */
+	private void initAnimation(){				
+		mTransformer = new CanvasTransformer(){
+			@Override
+			public void transformCanvas(Canvas canvas, float percentOpen) {
+				canvas.translate(0, canvas.getHeight() * (1 - interp.getInterpolation(percentOpen)));		
+			}		
+		};
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			//显示左边
+//			toggle();
+			//显示右边
+			getSlidingMenu().showSecondaryMenu();
+			return true;		
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+}
